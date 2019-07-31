655e3a8ef1a036cb05d934b1b76ad6ca35a62f88
abiolakunle <olakunle2abiola@gmail.com>
2019-07-31 14:28:47 -0700

    changed auth value from isLogged in to true in order to access dashboard

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PeiHeading from '../elements/PeiHeading';
+import Nav from './Nav';
 import Styles from './index.module.css';
-const Dashboard = () => {
-  return (
-    <div>
-      <h1 className={Styles.heading}>DASHBOARD</h1>
-    </div>
-  );
-};
-export default Dashboard;
+
+export default class index extends Component {
+  state = {
+    showNav: false
+  };
+
+  render() {
+    return (
+      <div className={Styles.container}>
+        <header>
+          <button className={Styles.menubtn}>
+            <div className={Styles.btnline} />
+            <div className={Styles.btnline} />
+            <div className={Styles.btnline} />
+          </button>
+          <div className={this.state.showNav ? 'show' : 'close'}>
+            <Nav />
+          </div>
+        </header>
+        <div className="main">Main</div>
+      </div>
+    );
+  }
+}
