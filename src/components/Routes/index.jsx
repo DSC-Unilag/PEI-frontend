8b927ddcbbcd0e9599f7d7f9e2d2450e7e085c0d
Akin Aguda <lordtitan7@gmail.com>
2019-07-31 15:58:26 +0100

    added proptype validation

--- src/components/Routes/index.jsx
+++ src/components/Routes/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import Sign from '../Auth/SignUp';
 import Auth from '../Auth';
@@ -11,16 +13,37 @@ const ProtectedRoute = ({ path, component, auth, exact, to }) =>
   ) : (
     <Redirect to={to || '/signin'} />
   );
-const Routes = () => {
+ProtectedRoute.propTypes = {
+  path: PropTypes.string.isRequired,
+  component: PropTypes.func.isRequired,
+  auth: PropTypes.bool.isRequired,
+  exact: PropTypes.bool,
+  to: PropTypes.string
+};
+ProtectedRoute.defaultProps = {
+  exact: false,
+  to: '/signin'
+};
+const Routes = ({ isLoggedIn }) => {
   return (
     <>
       <Route path="/" exact component={TempLandingPage} />
-      <ProtectedRoute path="/dashboard" exact component={Dashboard} />
+      <ProtectedRoute
+        path="/dashboard"
+        exact
+        auth={isLoggedIn}
+        component={Dashboard}
+      />
       <Route path="/auth" exact component={Auth} />
       <Route path="/signup" exact component={Sign} />
       <Route path="/signin" exact render={() => <Sign signin />} />
     </>
   );
 };
-
-export default Routes;
+Routes.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
+const mapStateToProps = state => ({
+  isLoggedIn: state.isLoggedIn
+});
+export default connect(mapStateToProps)(Routes);
--- src/components/elements/PeiButton/index.jsx
+++ src/components/elements/PeiButton/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 const PeiButton = ({ children, type, onClick }) => (
+  // eslint-disable-next-line react/button-has-type
   <button type={type} onClick={onClick}>
     {children}
   </button>
