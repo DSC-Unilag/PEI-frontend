7d464e5959c28e04561627efb7b1eb598f78b883
timzprof <timilehin.olumofin@gmail.com>
2019-08-01 17:41:38 +0100

    Initialized Firestore DB

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
-const functions = require('firebase-functions');
+import * as functions from 'firebase-functions';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import { config } from 'dotenv';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//POST /signup - Store user,
+//POST /accounts - Create new account for the user,
+//GET /accounts - Get all accounts for a user,
+//POST /transfer - Iniialize Transfer,
+//GET /transfer/:id - Validate if account being sent to exists
+
+config();
+
+const {
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_KEY,
+  REACT_APP_FIREBASE_DOMAIN
+} = process.env;
+
+// Initialise Database
+firebase.initializeApp({
+  apiKey: REACT_APP_FIREBASE_KEY,
+  authDomain: REACT_APP_FIREBASE_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID
+});
+
+const db = firebase.firestore();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
