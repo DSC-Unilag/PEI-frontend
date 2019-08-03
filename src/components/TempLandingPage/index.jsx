81f8debe6a192b1c6f690d37ae2e698a08e2d852
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2019-08-03 11:22:29 +0100

    Header set-up

--- src/components/TempLandingPage/index.jsx
+++ src/components/TempLandingPage/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Styles from './index.module.css';
+var logo = "./undraw_convert_2gjv.png";
+
 const TempLandingPage = () => {
   return (
     <div>
-      <Link to="/dashboard">Dashboard</Link>
-      <br />
-      <Link to="/auth">Auth</Link>
+      <nav className={Styles.nav_bar}>
+        <p><Link to="Home">Home</Link></p> 
+        <p><Link to="/dashboard">Dashboard</Link></p>
+        <p><Link to="/auth">Auth</Link></p>
+      </nav>
+
+      <div className={Styles.front_content}>
+        <img src="\images\undraw_mobile_payments_edgf.svg" alt="Conversion"></img>
+      </div>
+
+      <div className="home_page_content">
+
+      </div>
+
     </div>
   );
 };
+
 export default TempLandingPage;
