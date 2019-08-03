c79ac0415e8719e82c834d70f040fdcfc1fbb03e
Akin Aguda <lordtitan7@gmail.com>
2019-08-03 02:18:01 +0100

    added more user elements and implemnented all api

--- src/components/elements/PeiInput/index.jsx
+++ src/components/elements/PeiInput/index.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Styles from './index.module.css';
 
-const PeiInput = ({ type, name, onKeyUp, placeholder, onClick, purple }) => {
+const PeiInput = ({
+  type,
+  name,
+  onKeyUp,
+  onKeyPress,
+  placeholder,
+  onClick,
+  purple
+}) => {
   return (
     <input
       type={type}
       className={purple ? Styles.purpleInput : Styles.input}
       name={name}
       onKeyUp={onKeyUp}
+      onKeyPress={onKeyPress}
       onClick={onClick}
       placeholder={placeholder}
     />
@@ -18,6 +27,7 @@ PeiInput.propTypes = {
   type: PropTypes.string,
   name: PropTypes.string,
   onKeyUp: PropTypes.func,
+  onKeyPress: PropTypes.func,
   placeholder: PropTypes.string,
   onClick: PropTypes.func,
   purple: PropTypes.bool
@@ -26,6 +36,7 @@ PeiInput.defaultProps = {
   type: 'text',
   name: null,
   onKeyUp: null,
+  onKeyPress: null,
   placeholder: null,
   onClick: null,
   purple: false
